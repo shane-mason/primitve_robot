d722e1e1e294ba0d7b1fd1b4996f3db9a8eec76e
Shane C Mason <shane.c.mason@gmail.com>
2023-12-30 09:51:52 -0800

    Refactored player state

--- scripts/RobotHero.gd
+++ scripts/RobotHero.gd
@@ -3,9 +3,13 @@ extends CharacterBody3D
 
 @export var speed = 10
 @export var acceleration = 0.1
+@export var damper = .5
 @export var bump_jump = .2
 @export var jump_velocity = 10
-@export var eye_ramp: Gradient;
+@export var eye_ramp: Gradient
+@export var body_rotation_speed = 5.0
+@export var body_lean_speed = .5
+@export var wind_down_damper = .01
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") + 16
@@ -13,96 +17,158 @@ var _eye_material
 var _eye_buffer = 0
 var dead = false
 
+enum MotionState{
+	idle,
+	move_left,
+	move_right,
+	jump,
+	jump_left,
+	jump_right,
+	paused,
+	unpaused,
+	wind_down
+}
+
+var _prev_state = MotionState.idle
+var _new_state = MotionState.idle
+
 func _ready():
 	$HintLabel.text = "A - Move Left\nB - Move Right"
 	$HintLabel.visible = true
 	$TimerHint.start()
-	
-	#_eye_material = StandardMaterial3D.new()
-	#_eye_material.roughness = .1
-	#$EyeMesh.set_surface_material(0, _eye_material)
+
 	$BodyParts/EyeMesh.mesh.material.albedo_color = eye_ramp.sample(0)
-	$SFXMotor.play()
-	
-	
-func _physics_process(delta):
-	# Add the gravity.
-	
-	if dead:
-		return
-	
-	if position.y < -60:
-		dead=true
-		$SFXFell.play()
-	
+	#$SFXMotor.play()
+
+func _handle_jump():
+	velocity.y = jump_velocity
+	#turn the lights on
+	_eye_buffer = 1
+	$BodyParts/EyeMesh.mesh.material.albedo_color = eye_ramp.sample(_eye_buffer)
+	$SFXJump.play()
+		
+
+func _handle_eyes():
+	#decrement eyes if we need to
 	if _eye_buffer > 0:
 		_eye_buffer -= .02
 		$BodyParts/EyeMesh.mesh.material.albedo_color = eye_ramp.sample(_eye_buffer)
+
+func _handle_move(direction):
+
+	if abs(velocity.x) < speed:
+		velocity.x += direction.x * acceleration
+
+func _handle_dampen():
+	velocity.x = move_toward(velocity.x, 0, damper)
+
+func _handle_gravity(delta):
+	velocity.y -= gravity * delta
+
+func _handle_wheel():
+	$BodyParts/WheelMesh.rotation_degrees.x += velocity.x;
+	match _new_state:
+		MotionState.move_left, MotionState.move_right:
+			$BodyParts/WheelParticles.emitting = true
+		_:
+			$BodyParts/WheelParticles.emitting = false
+
+func _handle_wind_down():
+	$BodyParts/WheelParticles.emitting = false
+	velocity.x = move_toward(velocity.x, 0, wind_down_damper)
+	print(velocity.x)
+	move_and_slide()
+
+func _handle_rotation():
+	match _new_state:
+		MotionState.jump_right, MotionState.move_right:
+			if $BodyParts.rotation_degrees.y > 0:
+				$BodyParts.rotation_degrees.y = maxf(0, $BodyParts.rotation_degrees.y-body_rotation_speed)
+			if $BodyParts.rotation_degrees.z > -10:
+				$BodyParts.rotation_degrees.z-=.5
+		MotionState.jump_left, MotionState.move_left:
+			if $BodyParts.rotation_degrees.y < 180:
+				$BodyParts.rotation_degrees.y = minf(180, $BodyParts.rotation_degrees.y+body_rotation_speed)
+			if $BodyParts.rotation_degrees.z > -10:
+				$BodyParts.rotation_degrees.z-=.5
+		_:
+			if $BodyParts.rotation_degrees.z < 0:
+				$BodyParts.rotation_degrees.z+=.5
+
 		
-	# Handle Jump.
+
+func _handle_state(delta):
+	match _new_state:
+		MotionState.paused:
+			return
+		MotionState.wind_down:
+			_handle_wind_down()
+			return
+	_prev_state = _new_state
+	
+	#determine new state
+	var _jump_requested = false
+	var _bump_jump_requested = false
+	var _move_requested = false
+	
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = jump_velocity
-		_eye_buffer = 1
-		$BodyParts/EyeMesh.mesh.material.albedo_color = eye_ramp.sample(_eye_buffer)
-		#https://sfxr.me/#1FEzRygKcVnK7ktUYatAg19J3oBpQYvMiBw8D8mXZ9sa5b1EVE4GQTTMkdWruiPr1H5syEH2Nqpkr2D54FEHpkH36iEL6zGbmCbtRh5gbwjtjGcfF1L4XkhuV
-		$SFXJump.play()
+		_jump_requested = true
 	elif Input.is_action_pressed("move_jump"):
+		_bump_jump_requested = true
 		velocity.y += bump_jump
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		if abs(velocity.x) < speed:
-			velocity.x += direction.x * acceleration
 		
-	else:
-		velocity.x = move_toward(velocity.x, 0, .5)
-	
-	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-		$BodyParts/WheelParticles.emitting = false
-		$SFXMotor.pitch_scale = maxf( .50, $SFXMotor.pitch_scale-.04) 
-	else:
-		if abs(velocity.x) > 0:
-			if $SFXMotor.pitch_scale < 1:
-				$SFXMotor.pitch_scale += .04
+		_move_requested = true
+		if is_on_floor() and not _jump_requested:
+			if direction.x > 0:
+				_new_state = MotionState.move_right
+			else:
+				_new_state = MotionState.move_left
 		else:
-			if $SFXMotor.pitch_scale > 0:
-				$SFXMotor.pitch_scale -= .03
-		
-		
-		if absf(velocity.x) > .1:
-			$BodyParts/WheelParticles.emitting = true
-		else:
-			$BodyParts/WheelParticles.emitting = false
-		
-	
-	if velocity.x > 0:
-		$BodyParts.rotation_degrees.y = 0
-		if $BodyParts.rotation_degrees.z > -10:
-			$BodyParts.rotation_degrees.z-=.5
-		
-	elif velocity.x < 0:
-		$BodyParts.rotation_degrees.y = 180
-		if $BodyParts.rotation_degrees.z > -10:
-			$BodyParts.rotation_degrees.z-=.5
-		
+			if direction.x > 0:
+				_new_state = MotionState.jump_right
+			else:
+				_new_state = MotionState.jump_left
 	else:
-		if $BodyParts.rotation_degrees.z < 0:
-			$BodyParts.rotation_degrees.z+=.5
+		if not is_on_floor() or _jump_requested:
+			_new_state = MotionState.jump
+		else:
+			_new_state = MotionState.idle
 		
-	$BodyParts/WheelMesh.rotation_degrees.x += velocity.x;
-
+	if _move_requested:
+		_handle_move(direction)
+	else:
+		_handle_dampen()
+	if _jump_requested:
+		_handle_jump()
+		
+	_handle_gravity(delta)
+	_handle_eyes()
+	_handle_wheel()
+	_handle_rotation()
 	move_and_slide()
+
+func _physics_process(delta):
+	_handle_state(delta)
+			
 
 func show_hint(hint_text):
 	$HintLabel.text = hint_text
 	$HintLabel.visible = true
 	$TimerHint.start()
+
+func pause():
+	_new_state = MotionState.paused
+	
+func unpause():
+	_new_state = MotionState.unpaused
+	
+func wind_down():
+	_new_state = MotionState.wind_down
 	
 func _on_timer_hint_timeout():
 	$HintLabel.visible = false
--- scripts/world.gd
+++ scripts/world.gd
@@ -11,10 +11,24 @@ func _process(delta):
 	pass
 
 func level_end():
-	pass
-
+	$RobotHero.wind_down()
+	$Timers/LevelEndTimer.start()
+	
 func player_dead():
-	pass
+	get_tree().reload_current_scene()
 	
 func _on_start_music_timeout():
 	$SFX/SFXMusic.play()
+
+func _on_floor_boundary_body_entered(body):
+	if body.name == "RobotHero":
+		$SFX/SFXFell.play()
+		$Timers/DeadTimer.start()
+
+
+func _on_dead_timer_timeout():
+	player_dead()
+
+
+func _on_level_end_timer_timeout():
+	get_tree().reload_current_scene()
